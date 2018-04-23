66cfbb63295184886649e8f36221992a393701b0
Tom <tj@tjej.uk>
2018-04-23 20:21:01 +0100

    Add project files.

---
+++ ft8push/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ft8push
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
